f22d29dd6ffc9e73e377f059ff348443ea1111ee
Peter Wendrich <pwsoft@syntiac.com>
2023-02-20 22:00:56 +0100

    FPGACHESS updated board file only shows starting position no move logic yet.

--- rtl/designs/fpgachess/fpgachess_board.vhd
+++ rtl/designs/fpgachess/fpgachess_board.vhd
@@ -20,48 +20,50 @@ end entity;
 
 architecture rtl of fpgachess_board is
 	signal vid_piece_reg : piece_t := (others => '0');
+
+	type board_t is array(0 to 63) of piece_t;
+	signal display_board_reg : board_t := (
+			piece_white & piece_rook, piece_white & piece_knight, piece_white & piece_bishop, piece_white & piece_queen, piece_white & piece_king, piece_white & piece_bishop, piece_white & piece_knight, piece_white & piece_rook,
+			piece_white & piece_pawn, piece_white & piece_pawn, piece_white & piece_pawn, piece_white & piece_pawn, piece_white & piece_pawn, piece_white & piece_pawn, piece_white & piece_pawn, piece_white & piece_pawn,
+			piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none,
+			piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none,
+			piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none,
+			piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none, piece_white & piece_none,
+			piece_black & piece_pawn, piece_black & piece_pawn, piece_black & piece_pawn, piece_black & piece_pawn, piece_black & piece_pawn, piece_black & piece_pawn, piece_black & piece_pawn, piece_black & piece_pawn,
+			piece_black & piece_rook, piece_black & piece_knight, piece_black & piece_bishop, piece_black & piece_queen, piece_black & piece_king, piece_black & piece_bishop, piece_black & piece_knight, piece_black & piece_rook
+		);
+	signal display_col0_reg : piece_t := piece_white & piece_none;
+	signal display_col1_reg : piece_t := piece_white & piece_none;
+	signal display_col2_reg : piece_t := piece_white & piece_none;
+	signal display_col3_reg : piece_t := piece_white & piece_none;
+	signal display_col4_reg : piece_t := piece_white & piece_none;
+	signal display_col5_reg : piece_t := piece_white & piece_none;
+	signal display_col6_reg : piece_t := piece_white & piece_none;
+	signal display_col7_reg : piece_t := piece_white & piece_none;
 begin
 	vid_piece <= vid_piece_reg;
 
 	process(clk)
 	begin
 		if rising_edge(clk) then
-			case vid_row & vid_col is
-			when "000000" => vid_piece_reg <= piece_white & piece_rook;
-			when "000001" => vid_piece_reg <= piece_white & piece_knight;
-			when "000010" => vid_piece_reg <= piece_white & piece_bishop;
-			when "000011" => vid_piece_reg <= piece_white & piece_queen;
-			when "000100" => vid_piece_reg <= piece_white & piece_king;
-			when "000101" => vid_piece_reg <= piece_white & piece_bishop;
-			when "000110" => vid_piece_reg <= piece_white & piece_knight;
-			when "000111" => vid_piece_reg <= piece_white & piece_rook;
-			when "001000" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001001" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001010" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001011" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001100" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001101" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001110" => vid_piece_reg <= piece_white & piece_pawn;
-			when "001111" => vid_piece_reg <= piece_white & piece_pawn;
+			display_col0_reg <= display_board_reg(to_integer(vid_row & "000"));
+			display_col1_reg <= display_board_reg(to_integer(vid_row & "001"));
+			display_col2_reg <= display_board_reg(to_integer(vid_row & "010"));
+			display_col3_reg <= display_board_reg(to_integer(vid_row & "011"));
+			display_col4_reg <= display_board_reg(to_integer(vid_row & "100"));
+			display_col5_reg <= display_board_reg(to_integer(vid_row & "101"));
+			display_col6_reg <= display_board_reg(to_integer(vid_row & "110"));
+			display_col7_reg <= display_board_reg(to_integer(vid_row & "111"));
 
-			when "110000" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110001" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110010" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110011" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110100" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110101" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110110" => vid_piece_reg <= piece_black & piece_pawn;
-			when "110111" => vid_piece_reg <= piece_black & piece_pawn;
-			when "111000" => vid_piece_reg <= piece_black & piece_rook;
-			when "111001" => vid_piece_reg <= piece_black & piece_knight;
-			when "111010" => vid_piece_reg <= piece_black & piece_bishop;
-			when "111011" => vid_piece_reg <= piece_black & piece_queen;
-			when "111100" => vid_piece_reg <= piece_black & piece_king;
-			when "111101" => vid_piece_reg <= piece_black & piece_bishop;
-			when "111110" => vid_piece_reg <= piece_black & piece_knight;
-			when "111111" => vid_piece_reg <= piece_black & piece_rook;
-			when others =>
-				vid_piece_reg <= piece_white & piece_none;
+			case vid_col is
+			when "000" => vid_piece_reg <= display_col0_reg;
+			when "001" => vid_piece_reg <= display_col1_reg;
+			when "010" => vid_piece_reg <= display_col2_reg;
+			when "011" => vid_piece_reg <= display_col3_reg;
+			when "100" => vid_piece_reg <= display_col4_reg;
+			when "101" => vid_piece_reg <= display_col5_reg;
+			when "110" => vid_piece_reg <= display_col6_reg;
+			when "111" => vid_piece_reg <= display_col7_reg;
 			end case;
 		end if;
 	end process;
