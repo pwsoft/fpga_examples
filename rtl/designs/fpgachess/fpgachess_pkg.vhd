bb4133ac9fee518802f7b9373cfb21bbd0937208
Peter Wendrich <pwsoft@syntiac.com>
2023-03-01 01:59:12 +0100

    FPGACHESS Extended package with extpiece type and conversion routines. Signals empty squares differently and has separate color flags so search logic becomes simpler. However for storage and display the piece_t is more efficient so both types will see use.

--- rtl/designs/fpgachess/fpgachess_pkg.vhd
+++ rtl/designs/fpgachess/fpgachess_pkg.vhd
@@ -34,6 +34,7 @@ use ieee.numeric_std.all;
 
 package fpgachess_pkg is
 	subtype piece_t is unsigned(3 downto 0);
+	subtype extpiece_t is unsigned(4 downto 0);
 	constant piece_white  : std_logic := '0';
 	constant piece_black  : std_logic := '1';
 	constant piece_empty  : unsigned(3 downto 0) := "0000";
@@ -44,7 +45,38 @@ package fpgachess_pkg is
 	constant piece_rook   : unsigned(2 downto 0) := "100";
 	constant piece_queen  : unsigned(2 downto 0) := "101";
 	constant piece_king   : unsigned(2 downto 0) := "110";
+
+	constant ext_empty   : unsigned(4 downto 0) := "00000";
+	constant ext_wpawn   : unsigned(4 downto 0) := "10001";
+	constant ext_wbishop : unsigned(4 downto 0) := "10010";
+	constant ext_wknight : unsigned(4 downto 0) := "10011";
+	constant ext_wrook   : unsigned(4 downto 0) := "10100";
+	constant ext_wqueen  : unsigned(4 downto 0) := "10101";
+	constant ext_wking   : unsigned(4 downto 0) := "10110";
+	constant ext_bpawn   : unsigned(4 downto 0) := "01001";
+	constant ext_bbishop : unsigned(4 downto 0) := "01010";
+	constant ext_bknight : unsigned(4 downto 0) := "01011";
+	constant ext_brook   : unsigned(4 downto 0) := "01100";
+	constant ext_bqueen  : unsigned(4 downto 0) := "01101";
+	constant ext_bking   : unsigned(4 downto 0) := "01110";
+
+	function to_piece(p : in extpiece_t) return piece_t;
+	function to_extpiece(p : in piece_t) return extpiece_t;
 end package;
 
 package body fpgachess_pkg is
+	function to_piece(p : in extpiece_t) return piece_t is
+	begin
+		return p(3 downto 0);
+	end function;
+
+	function to_extpiece(p : in piece_t) return extpiece_t is
+		variable white_flag : std_logic;
+	begin
+		white_flag := '0';
+		if (p(3) = piece_white) and (p(2 downto 0) /= piece_none) then
+			white_flag := '1';
+		end if;
+		return white_flag & p;
+	end function;
 end package body;
